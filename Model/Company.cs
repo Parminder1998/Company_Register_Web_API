974353b5af6514c078578e9a4134b167919c85a8
Admin <>
2021-09-17 10:23:54 +1200

    Add project files.

---
+++ Model/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_Register_Web_API.Model
+{
+    public class Company // company class 
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } // company name 
+
+        public int AnnualNetProfit {get; set;} // anual profit
+
+        public int NumberOfEmployees { get; set; } //no. of employess
+
+
+
+    }
+}
